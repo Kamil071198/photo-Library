8678eb8af6a20c78c368ffdba1b1895ed2ac9f3d
Kamil071198 <kamilchavda@gmail.com>
2024-05-31 15:02:25 +0530

    New

---
+++ program.js
@@ -0,0 +1,190 @@
+// Write a JavaScript function to calculate the sum of two numbers.
+// let addition = (a, b) => a + b;
+
+// let num1 = 25;
+// let num2 = 25;
+// let sum = addition(num1, num2);
+// console.log(sum);
+
+
+
+// Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards).
+// Write a JavaScript program to reverse a given string.
+
+// function reverseString(str) {
+
+
+//     let newString = "";
+//     for (let i = str.length - 1; i >= 0; i--) 
+//     {
+//         newString += str[i];
+//     }
+//     return newString;
+//     }
+    
+//     const string = prompt('Enter a string:');
+//     const result = reverseString(string);
+//     console.log(result);
+        
+      
+    
+
+// // Write a JavaScript program to convert a string to title case (capitalize the first letter of each word).
+
+// Q1. Create a variable called a and assign 10 as a value.
+
+// let = 10;
+
+// Q2. Create a variable called name and assign it to “felix its” as a value.
+
+// let = "felix its";
+
+// Q3. Assign x to 10 and y = 20 and print their sum in the console.
+
+// let x = 10 ;
+// let y = 20 ;
+// console.log(x+y);
+
+// Q4. Assign Felix its to the variable and print the variable
+
+// let x = "felix its";
+// console.log(x);
+
+// Q5. Print “Hello world” in the console
+
+// console.log("Hello world");
+
+// Q1. Return the sum of a and b
+
+// function  sum(a,b){
+
+//  a+
+// ;
+// }
+
+// Q3. Use if else wisely
+
+//  if (a>b){
+//     console.log("a is greater than b");
+// }
+// else
+// {
+//     console.log("b is greater than a");
+//  }
+
+// Q4. Use continue statement and print i
+
+// for (let i = 0; i < 11; i++) {
+//     if (i % 2 == 0);
+
+// 
+//     console.log(i);
+//
+// }
+
+// Q1. Create a for loop that prints number 1 to 10.
+
+// for(let  i=1
+// ; i<=10
+// ; i++){
+//     console.log(i);
+// 
+// }
+
+// Q2. Crete a while loop that runs when i is less than 7
+
+// let i=0;
+// while(i<7)
+// ){
+//     console.log(i);
+// }
+
+// Q3. Make this loop skip the iteration when k = 4
+
+// for( let k=0 ; k<5; k++){
+//     if(k==4){
+    //  continue;
+// 
+//     }
+//     console.log(k);
+// }
+
+// Q4. Break the loop when k is equal to 3.
+
+// for( let k=0 ; k<5; k++){
+//     if(k==3) {
+        //    break;
+// ;
+//     }
+//     console.log(k);
+// }
+
+// Q5. Print the table of two by adding some expression
+
+for(let  i = 1; i<=10 ; i++){
+    console.log(i * 5);
+ }
+
+// Q6. Iterate through the courses using for-in loop
+
+// const courses = {
+//     firstCourse: 'JavaScript',
+//     secondCourse: 'React',
+//     thirdCourse: 'Angular'
+// };
+// let value = '';
+//  for (let
+// in
+// ){
+//     value += courses[item] + " ";
+//     console.log(value);
+// }
+
+// Q1. Add the two numbers a and b and print the result by using the operator
+// a=10; 
+// b=10;
+// console.log(a, b);
+
+// Q2. Print the reminder for 5 when divided by 4
+
+// console.log(54);
+
+// Q3. Use the correct relational operator for printing the result
+
+// let a =90; b= 78;
+// if(a > b){
+//     console.log("a is greater than b");}
+// else {
+//     console.log("b is greater");
+// }
+
+// Q4. Add 4 to a
+
+// let a = 5;
+// let a = 4;
+
+// Q5. Check whether a and b are equal or not
+
+// let a = 7;
+// let b = 7;
+// if(a==b){
+//     console.log(" a is equal to b");
+// }
+// else{
+    // console.log(" a is  not equal to b");
+// }
+
+// Q1. Call the function
+
+// function myFunc(){
+//     console.log(“function is calling”);
+// }                                        
+//  myFunc();
+
+// Q2. Create a function named Geeks accepting no props
+
+// {
+    // function Geeks( );{
+//     console.log("hello Geeks");
+// }
+// Geeks();
